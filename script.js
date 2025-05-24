c9caf30c0e1cfe63da420d9108de39f4e140e596
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-05-25 01:42:13 +0900

    Complete implementation
    Actually forgot to atomic commit again

--- script.js
+++ script.js
@@ -1,103 +1,110 @@
-function playGame(count){
-    const WIN = "You win!"
-    const LOSE = "You lose!"
-    const DRAW = "Draw!"
+const WIN = "You win!"
+const LOSE = "You lose!"
+const DRAW = "Draw!"
+
+// Map '0' to "rock", '1' to "paper", '2' to "scissors"
+const arr = ["Rock", "Paper", "Scissors"];
+
+
+// Total rounds
+let count = 0;
+
+// Initialize both score
+let humanScore = computerScore = 0;
+
+// Display components
+const resultDiv = document.querySelector('#result');
+const roundResultDiv = document.querySelector('#roundResult');
+const scoreBoard = document.querySelector('#scoreBoard');
+const humanScoreSpan = document.querySelector('#humanScore');
+const computerScoreSpan = document.querySelector('#comScore')
+
+function playRound(evt){
+    const computerChoice = getComputerChoice();
+    const humanChoice = getHumanChoice(evt.target.textContent);
+
+    const result = decideWinner(humanChoice, computerChoice);
+
+    roundResultDiv.textContent = getRoundResultMessage(result, humanChoice, computerChoice);
     
-    // Map '0' to "rock", '1' to "paper", '2' to "scissors"
-    const arr = ["rock", "paper", "scissors"];
-
-    function getComputerChoice(){
-        return Math.floor(Math.random() * 3);
+    if (result === WIN){
+        humanScore++;
+    } else if (result === LOSE){
+        computerScore++;
     }
 
-    function getHumanChoice(){
-        const choice = prompt("Rock Paper Scissors Game! Select your choice.", "'rock' or 'paper' or 'scissors'");
+    displayRunningScore(humanScore, computerScore);
 
-        // Return corresponding index from 'arr'
-        for (let i = 0, len = arr.length; i < len; i++){
-            if (choice === arr[i]){
-                return i;
-            }
-        }
-        return null;
+    if ((++count) === 5){
+        const gameResultDiv = finishGame();
+        resultDiv.appendChild(gameResultDiv);
     }
-
-    function playRound(humanChoice, computerChoice){
-        // Get capitalized string of both sides' choice
-        const human = arr[humanChoice].at(0).toUpperCase() + arr[humanChoice].slice(1);
-        const com = arr[computerChoice].at(0).toUpperCase() + arr[computerChoice].slice(1);
-
-        const result = decideWinner(humanChoice, computerChoice);
-
-        let resultMessage;
-
-        switch (result){
-            case WIN:
-                resultMessage = `${result} ${human} beats ${com}`; break;
-            case LOSE:
-                resultMessage = `${result} ${com} beats ${human}`; break;
-            case DRAW:
-                resultMessage = `${result} ${com} ties with ${human}`; break;
-        }
-
-        console.log(resultMessage);
-
-        return result;
-    }
-
-    function decideWinner(humanChoice, computerChoice){
-        // Process 'computerChoice': map '2' to "rock", '1' to "paper", '0' to "scissors"
-        processedComputerChoice = -computerChoice + 2;
-
-        switch ((humanChoice + processedComputerChoice) % 3){
-            case 0:
-                return WIN;
-            case 1:
-                return LOSE;
-            case 2:
-                return DRAW;
-        }
-    }
-
-    // Initialize both score
-    let humanScore = computerScore = 0;
-
-    for (let i = 0; i < count; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        const roundResult = playRound(humanSelection, computerSelection);
-
-        // Manipulate score according to result of round
-        if (roundResult === WIN){
-            humanScore++;
-        } else if (roundResult === LOSE){
-            computerScore++;
-        }
-    }
-
-    // Total score and game's result
-    let gameResult = 
-        `Your score: ${humanScore}\nComputer's score: ${computerScore}`;
-
-    if (humanScore > computerScore){
-        gameResult += "\n" + WIN;
-    } else if (humanScore < computerScore){
-        gameResult += "\n" + LOSE;
-    } else {
-        gameResult += "\n" + DRAW;
-    }
-
-    console.log(gameResult);
 }
 
-
-playGame(5);
-
+document.querySelectorAll('.humanChoice').forEach(element => element.addEventListener('click', playRound));
 
 
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3);
+}
 
+function getHumanChoice(choice){
+    // Return corresponding index from 'arr'
+    for (let i = 0, len = arr.length; i < len; i++){
+        if (choice === arr[i]){
+            return i;
+        }
+    }
+    return null;
+}
 
+function decideWinner(humanChoice, computerChoice){
+    // Process 'computerChoice': map '2' to "rock", '1' to "paper", '0' to "scissors"
+    processedComputerChoice = -computerChoice + 2;
 
+    switch ((humanChoice + processedComputerChoice) % 3){
+        case 0:
+            return WIN;
+        case 1:
+            return LOSE;
+        case 2:
+            return DRAW;
+    }
+}
 
+function getRoundResultMessage(result, humanChoice, computerChoice){
+    const human = arr[humanChoice];
+    const com = arr[computerChoice];
+    
+    switch (result){
+        case WIN:
+            return `${result} ${human} beats ${com}`; 
+        case LOSE:
+            return `${result} ${com} beats ${human}`; 
+        case DRAW:
+            return `${result} ${com} ties with ${human}`; 
+    }
+}
 
+function displayRunningScore(humanScore, computerScore){
+    humanScoreSpan.textContent = humanScore;
+    computerScoreSpan.textContent = computerScore;
+}
+
+function finishGame(){
+    let gameResultMessage;
+    
+    if (humanScore > computerScore){
+        gameResultMessage = WIN;
+    } else if (humanScore < computerScore){
+        gameResultMessage = LOSE;
+    } else {
+        gameResultMessage = DRAW;
+    }
+    
+    document.querySelectorAll('.humanChoice').forEach(element => element.removeEventListener('click', playRound));
+
+    const container = document.createElement('div');
+    container.textContent = gameResultMessage;
+    return container;
+}
